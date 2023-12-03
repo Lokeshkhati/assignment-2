2abe57b17ece4ccb740bbeafb2b669116ac2e9a2
lokesh <lokeshkhati005@gmail.com>
2023-12-04 04:16:34 +0530

    add gray bg to selected row

--- components/Table.tsx
+++ components/Table.tsx
@@ -91,17 +91,15 @@ export default function Table({ data, currentPage }: any) {
                             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                 <IoIosSearch className="h-5 w-5 text-gray-400" aria-hidden="true" />
                             </div>
-                            <form onSubmit={getFilteredData} >
-                                <input
-                                    id="search"
-                                    name="search"
-                                    value={query}
-                                    onChange={(e) => setQuery(e.target.value)}
-                                    className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    placeholder="Search"
-                                    type="search"
-                                />
-                            </form>
+                            <input
+                                id="search"
+                                name="search"
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                                className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Search"
+                                type="search"
+                            />
                         </div>
                     </div>
                 </div>
@@ -164,7 +162,7 @@ export default function Table({ data, currentPage }: any) {
                                             (currentPage - 1) * TOTAL_ITEMS_PER_PAGE,
                                             currentPage * TOTAL_ITEMS_PER_PAGE
                                         ).map((item: any) => (
-                                            <tr key={item.email} >
+                                            <tr key={item.email} className={`${selectedRows.includes(item) ? 'bg-gray-200' : null}`} >
                                                 <td className="relative px-7 sm:w-12 sm:px-6">
                                                     <input
                                                         type="checkbox"
