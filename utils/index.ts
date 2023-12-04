a52616c6d23e352eccde5fef8251b9b01f8f02ec
lokesh <lokeshkhati005@gmail.com>
2023-12-04 17:57:13 +0530

    clean up

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,6 @@
-export const getSortedData = () => { }
-export const getFilteredData = () => { }
 
 export function classNames(...classes: any) {
     return classes.filter(Boolean).join(' ')
 }
 export const difference = (arr1, arr2) =>
-    arr1.filter(item => !arr2.includes(item));
+    arr1.filter((item: any) => !arr2.includes(item));
